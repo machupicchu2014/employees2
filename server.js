aa63b81db5792644e957ae71a2aa92fc28ef73e7
Matthew Cox <machupicchu2014@gmail.com>
2018-03-14 22:27:54 -0600

    Changed default port

--- server.js
+++ server.js
@@ -53,4 +53,4 @@ app.delete("/api/employees/:id", (req, res) => {
 });
 
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(8000, () => console.log('Server listening on port 3000!'));
